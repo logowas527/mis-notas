69ac2cd42e99005241483f9691d805c4ccad3812
logowas527 <125397351+logowas527@users.noreply.github.com>
2023-02-14 17:47:55 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'me',
-  host: 'localhost',
-  database: 'notepad',
-  password: 'password',
+  host: 'dpg-cflu1npa6gdjlmrf3hug-a',
+  database: 'notepad_zji2',
+  password: 'wdlyuoS1x7PDNcTZN3cHYfcuFOXqMESS',
   port: 5432,
 })
 
