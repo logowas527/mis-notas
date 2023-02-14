f938ed98c3a6ec51477c63ca3753ee52e0e0d598
logowas527 <erickjaviergg2000@gmail.com>
2023-02-14 17:36:38 -0600

    First commit

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("/", async (req, res) => {
     res.json(rows);
   });
   
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log("Puerto 3000");
   });
